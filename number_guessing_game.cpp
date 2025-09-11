ccdc81d931ccfc503690d5479ba3bc6ffd20c9c4
Anjali Tiwari <tanjali16aug@gmail.com>
2025-09-12 00:55:38 +0530

    xyz

---
+++ number_guessing_game.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+int main()
+{
+  int num;
+  int guess;
+  int tries;
+
+  srand(time(NULL));
+  num = (rand() % 100) + 1;
+
+  std::cout<<"********number guessing game ************\n";
+
+  do
+  {
+    std::cout<<"enter a guess between (1-100): ";
+    std::cin>>guess;
+    tries++;
+
+    if(guess>num)
+    {
+      std::cout<<"Too High!\n";
+    }
+    else if(guess < num)
+    {
+      std::cout<<"Too Low!\n";
+    }
+    else
+    {
+      std::cout<<"CORRECT! # of tries: "<<tries<<'\n';
+    }
+    
+  }while(guess!=num);
+
+  std::cout<<"********************\n";
+
+  return 0;
+
+}
